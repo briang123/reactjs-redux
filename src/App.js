78ea8079372b46b5e42b5b4be94bc776617efc0c
briang123 <briang.sandiego@gmail.com>
2018-12-06 12:36:46 -0800

    Completed redux implementation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+
 import Posts from './components/Posts';
 import PostForm from './components/Postform';
 
-class App extends Component { 
-  render() {
-    return (
-      <div className="App">
-        <PostForm/>
-        <hr/>
-        <Posts/>
-      </div>
-    );
-  }
+import store from './store';
+
+class App extends Component {
+	render() {
+		return (
+			<Provider store={store}>
+				<div className="App">
+					<PostForm />
+					<hr />
+					<Posts />
+				</div>
+			</Provider>
+		);
+	}
 }
 
 export default App;
--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,59 +1,73 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/postActions';
 
 class PostForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: "",
-            body:""
-        }
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: '',
+			body: ''
+		};
 
-    onChange(e) {
-        this.setState({[e.target.name]: e.target.value});
-    }
+		this.onChange = this.onChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
 
-    onSubmit(e) {
-        e.preventDefault();
-        const post = {
-            title: this.state.title,
-            body:this.state.body
-        }
+	onChange(e) {
+		this.setState({ [e.target.name]: e.target.value });
+	}
 
-        fetch('http://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            header: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(post) 
-        })
-        .then(res=>res.json())
-        .then(data=>console.log(data));
-    }
+	onSubmit(e) {
+		e.preventDefault();
 
-    render() {
-        return (
-            <div>
-            <h1>Add Post</h1>
-            <form onSubmit={this.onSubmit}>
-                <div>
-                    <label>Title:</label><br/>
-                    <input type="text" name="title" onChange={this.onChange} value={this.state.title}/>
-                </div>
-                <br/>
-                <div>
-                    <label>Body:</label><br/>
-                    <textarea name="body" onChange={this.onChange} value={this.state.body}/>
-                </div>
-                <br/>
-                <button type="submit">Submit</button>
-            </form>
-            <hr/>
-            </div>
-        )
-    }
+		const post = {
+			title: this.state.title,
+			body: this.state.body
+		};
+
+		this.props.createPost(post);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Add Post</h1>
+				<form onSubmit={this.onSubmit}>
+					<div>
+						<label>Title:</label>
+						<br />
+						<input
+							type="text"
+							name="title"
+							onChange={this.onChange}
+							value={this.state.title}
+						/>
+					</div>
+					<br />
+					<div>
+						<label>Body:</label>
+						<br />
+						<textarea
+							name="body"
+							onChange={this.onChange}
+							value={this.state.body}
+						/>
+					</div>
+					<br />
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+		);
+	}
 }
 
-export default PostForm
+PostForm.propTypes = {
+	createPost: PropTypes.func.isRequired
+};
+
+export default connect(
+	null,
+	{ createPost }
+)(PostForm);
